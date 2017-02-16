428c5947d93104f7ab0548b40357878c72825cb6
Kurt Weloth <kurtweloth@mail.weber.edu>
2017-02-16 17:02:07 +0000

    Changes

--- electrons_hw5_task2.c
+++ electrons_hw5_task2.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  electrons_hw5_task2.c
  *       	Usage:  ./electrons_hw5_task2.c
- *    Description:  
+ *    Description: Miles to Meters 
  *
  *        Version:  1.0
  *        Created:  02/14/2017 04:37:43 PM
@@ -18,11 +18,29 @@
 /* Main Program */
 int main(void)
 {
+	float mile;
+	float ending;
+	float increment;
+	float kilometer;
+
+	printf("Enter the starting mile distance: ");
+	scanf("%f", &mile);
+	printf("Enter the ending mile distance: ");
+	scanf("%f", &ending);
+	printf("Enter the distance increment: ");
+	scanf("%f", &increment);
+
+	printf("Miles   =   Kilometers\n");
+	while(mile <= ending)
+	{
+		kilometer = mile * 1.61;
+		printf("%5.2f       %10.2f\n", mile, kilometer);
+		mile += increment;
+	}
 
 	return 0;
 }
 
 
 /* Function Defenitions */
-
 
